56f998429898be5d00e2ad21a92f10e6cb0d0a8d
Elipol <illlyapolll@gmail.com>
2017-01-31 21:00:44 +0200

    transaction keeper

--- src/main/java/com/ilyapolyanskiy/practice/module02/task022/TactionKeeper.java
+++ src/main/java/com/ilyapolyanskiy/practice/module02/task022/TactionKeeper.java
@@ -4,14 +4,12 @@ package com.ilyapolyanskiy.practice.module02.task022;
  * Created by GVNcoder on 31.01.2017.
  */
 public class TactionKeeper {
-    private double commission, balanceAfter;
-    private int withdrawal;
     public double commissionCalc(int withdrawal) {
-        commission = (double) withdrawal * 0.05;
+        double commission = (double) withdrawal * 0.05;
         return commission;
     }
     public double balanceCalc(int balance, int withdrawal) {
-        balanceAfter = (double) balance - withdrawal * 0.05 - withdrawal;
+        double balanceAfter = (double) balance - withdrawal * 0.05 - withdrawal;
         return balanceAfter;
     }
 }
---
+++ src/main/java/com/ilyapolyanskiy/practice/module02/task023/AuthorisationTactionRunner.java
@@ -0,0 +1,24 @@
+package com.ilyapolyanskiy.practice.module02.task023;
+
+import com.ilyapolyanskiy.practice.module02.task022.TactionKeeper;
+
+/**
+ * Created by GVNcoder on 31.01.2017.
+ */
+public class AuthorisationTactionRunner {
+    public static void main(String[] args) {
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+        String ownerName = "Ann";
+        int withdrawal = 100;
+        int index;
+
+        for (index = 0; index < ownerNames.length; index++)
+            if (ownerNames[index] == ownerName)
+                break;
+
+        TactionKeeper nameKeeper = new TactionKeeper();
+        if (nameKeeper.balanceCalc(balances[index], withdrawal) > 0)
+            System.out.println(ownerName + " " + withdrawal + " " + " " + nameKeeper.balanceCalc(balances[index], withdrawal));
+    }
+}
