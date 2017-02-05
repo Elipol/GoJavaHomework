b1e9df301b52f6f10b84fb3de47562f7602c9d78
ignipolari <illlyapolll@gmail.com>
2017-02-05 21:26:17 +0200

    ultimate

--- src/main/java/com/ilyapolyanskiy/practice/module02/task021/ArrayCalculator.java
+++ src/main/java/com/ilyapolyanskiy/practice/module02/task021/ArrayCalculator.java
@@ -4,55 +4,62 @@ package com.ilyapolyanskiy.practice.module02.task021;
  * Created by GVNcoder on 30.01.2017.
  */
 public class ArrayCalculator{
-    private int iMin, iMax, iCount = 0, iMultiple = 1;
-    private double dMin, dMax, dCount = 0, dMultiple = 1;
-
-    public ArrayCalculator(int array[]){
-        iMin = array[0];
-        iMax = array[0];
-        for (int i = 0; i < array.length; i++){
-            iCount += array[i];
-            if (iMin > array[i])
-                iMin = array[i];
-            else if (iMax < array[i])
-                iMax = array[i];
-            iMultiple *= array[i];
-        }
-    }
-    public ArrayCalculator(double array[]){
-        dMin = array[0];
-        dMax = array[0];
-        for (int i = 0; i < array.length; i++){
-            dCount += array[i];
-            if (dMin > array[i])
-                dMin = array[i];
-            else if (dMax < array[i])
-                dMax = array[i];
-            dMultiple *= array[i];
-        }
-    }
     public void sum(int array[]){
+        int iCount = 0;
+        for (int i = 0; i < array.length; i++)
+            iCount += array[i];
         System.out.println("\nСумма элементов массива: " + iCount);
     }
     public void sum(double array[]){
+        double dCount = 0;
+        for (int i = 0; i < array.length; i++)
+            dCount += array[i];
         System.out.println( "\nСумма элементов массива: " + dCount);
     }
     public void minDivMax(int array[]) {
-        System.out.println("\nДеление min элемента на max: " + (double)iMin / iMax);
+        int iMin = 0, iMax = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (iMin >= array[i])
+                iMin = array[i];
+            else if(iMax <= array[i])
+                iMax = array[i];
+        }
+        System.out.println("\nmin элемент: " +iMin + "\nmax элемент: " + iMax);
     }
     public void minDivMax(double array[]){
-        System.out.println( "\nДеление min элемента на max: " +  dMin / dMax);
+        double dMin = 0, dMax = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (dMin >= array[i])
+                dMin = array[i];
+            else if(dMax <= array[i])
+                dMax = array[i];
+        }
+        System.out.println("\nmin элемент: " +dMin + "\nmax элемент: " + dMax);
     }
     public void maxPositive(int arr[]){
+        int iMax = arr[0];
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > iMax)
+                iMax = arr[i];
         System.out.println("\nНайбольший положительный элемент: " + iMax);
     }
     public void maxPositive(double array[]){
+        double dMax = array[0];
+        for (int i = 0; i < array.length; i++)
+            if (array[i] > dMax)
+                dMax = array[i];
         System.out.println("\nНайбольший положительный элемент: " + dMax);
     }
     public void multiplication(int array[]){
+        int iMultiple = 1;
+        for (int i = 0; i < array.length; i++)
+                iMultiple *= array[i];
         System.out.println("\nПроизведение всех элементов: " + iMultiple);
     }
     public void multiplication(double array[]){
+        double dMultiple = 1;
+        for (int i = 0; i < array.length; i++)
+            dMultiple *= array[i];
         System.out.println("\nПроизведение всех элементов: " + dMultiple);
     }
     public void modulus(int array[]){
@@ -64,6 +71,11 @@ public class ArrayCalculator{
 
     public void secondLargest(int array[]) {
         int iSecondMax = array[0], numer = 1;
+        int iMax = array[0];
+        for (int i = 0; i < array.length; i++)
+                if (array[i] > iMax) {
+                    iMax = array[i];
+                }
         for (int i = 0; i < array.length; i++)
             if (iSecondMax < array[i] && array[i] != iMax) {
                 iSecondMax = array[i];
@@ -72,10 +84,15 @@ public class ArrayCalculator{
         System.out.println("\nВторой по величине элемент = " + iSecondMax + " №" + numer);
     }
     public void secondLargest(double array[]) {
+        double dMax = array[0];
         double dSecondMax = array[0];
         int numer = 1;
         for (int i = 0; i < array.length; i++)
-            if (dSecondMax < array[i] && dSecondMax != iMax){
+                if (array[i] > dMax) {
+                    dMax = array[i];
+                }
+        for (int i = 0; i < array.length; i++)
+            if (dSecondMax < array[i] && dSecondMax != dMax){
                 dSecondMax = array[i];
                 numer = i + 1;
             }
--- src/main/java/com/ilyapolyanskiy/practice/module02/task021/Main.java
+++ src/main/java/com/ilyapolyanskiy/practice/module02/task021/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) {
         double[] mineDoubleArray = {-5.67,5.15,6.17,8.1,1.6,3.3,2.3,3.8,6.4,8.17};
         int[] mineArray  = {6,4,1,-7,-12,34,2,8,0,11};
-        ArrayCalculator minArCalcul = new ArrayCalculator(mineArray);
-        ArrayCalculator minDArCalcul = new ArrayCalculator(mineDoubleArray);
+        ArrayCalculator minArCalcul = new ArrayCalculator();
         System.out.println("====================================|");
         System.out.println("Результаты int массива");
         minArCalcul.sum(mineArray);
@@ -19,12 +18,12 @@ public class Main {
         minArCalcul.secondLargest(mineArray);
         System.out.println("\n====================================|");
         System.out.println("Результаты double массива");
-        minDArCalcul.sum(mineDoubleArray);
-        minDArCalcul.minDivMax(mineDoubleArray);
-        minDArCalcul.maxPositive(mineDoubleArray);
-        minDArCalcul.multiplication(mineDoubleArray);
-        minDArCalcul.modulus(mineDoubleArray);
-        minDArCalcul.secondLargest(mineDoubleArray);
+        minArCalcul.sum(mineDoubleArray);
+        minArCalcul.minDivMax(mineDoubleArray);
+        minArCalcul.maxPositive(mineDoubleArray);
+        minArCalcul.multiplication(mineDoubleArray);
+        minArCalcul.modulus(mineDoubleArray);
+        minArCalcul.secondLargest(mineDoubleArray);
         System.out.println("\n====================================|");
     }
 }
